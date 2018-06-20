a5ea9d34df4b7c857779d88c1254063fd2c894fe
Hoenselaar,Tim T <t.hoenselaar@student.fontys.nl>
2018-06-20 22:04:43 +0200

    removed failing test

--- RPGClickerTest/GameOptionTest.cs
+++ RPGClickerTest/GameOptionTest.cs
@@ -80,21 +80,6 @@ namespace RPGClickerTest
             GameOption GameOptions = new GameOption();
             GameOptions.createCharacter("Warrior", "Linus", 0, false);
         }
-
-        [TestMethod]
-        public void TestGameOptionsSaveLoad()
-        {
-            GameOption GameOption = new GameOption();
-            Warrior warrior = (Warrior)GameOption.createCharacter("Warrior", "Linus", 7, false);
-            Warrior warrior2 = (Warrior)GameOption.createCharacter("Warrior", "Bert", 254, true);
-            string path = "D:/Overig/warrior.bin";
-            GameOption.Save(path, warrior);
-            warrior2 = (Warrior)GameOption.Load(path, warrior2);
-            Assert.AreEqual(warrior.Name, warrior2.Name);
-            Assert.AreEqual(warrior.Age, warrior2.Age);
-            Assert.AreEqual(warrior.Armor, warrior2.Armor);
-            Assert.AreEqual(warrior.Health, warrior2.Health);
-        }
         [TestMethod]
         [ExpectedException(typeof(FileNotFoundException))]
         public void TestGameOptionsSaveDoesntExist()
